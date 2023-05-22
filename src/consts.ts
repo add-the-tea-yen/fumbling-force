e94541a2909711531b59dc2b50059de48dc9e496
addtheteayen <adityan041414@gmail.com>
2023-05-23 00:37:26 +0530

    addtheteayen (Adityan)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'addtheteayen';
+export const SITE_TITLE = 'addtheteayen (Adityan)';
 export const SITE_DESCRIPTION = 'Blog, Some Photography and a reseume in here somewhere';
